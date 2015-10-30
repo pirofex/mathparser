c7b8065a02698d106f0e7232390fdef4bf3b9371
Lukas Feldmann <mail@lukas-feldmann.de>
2015-10-30 15:13:48 +0100

    better code structure

--- mathparser/main.cpp
+++ mathparser/main.cpp
@@ -2,6 +2,41 @@
 #include <iomanip> 
 #include "includes/Parser.h"
 
+void process_command_line(const std::string& input)
+{
+	if (input == "/help")
+	{
+		std::cout
+			<< "\n"
+			<< "  sin(x)    - calculates the sinus of x  \n"
+			<< "  cos(x)    - calculates the cosinus of x\n"
+			<< "  tan(x)    - calculates the tangens of x\n"
+			<< "  log(x)    - calculates the natural logarithm of x\n\n"
+			<< "  sqrt(x)   - calculates the square root of x\n"
+			<< "  pow(x, y) - calculates x high y\n\n"
+			<< "  digsum(x) - calculates the digit sum of x\n"
+			<< "  ceil(x)   - rounds x upward\n"
+			<< "  floor(x)  - rounds x downward\n\n"
+			<< "  e         - eularian number\n"
+			<< "  pi        - circular number pi\n"
+			<< "\n";
+	}
+
+	if (input == "/clear")
+	{
+		#ifdef _WIN32
+			system("cls");
+		#else
+			system("clear");
+		#endif
+	}
+
+	if (input == "/maxval")
+	{
+		std::cout << "-> " << std::numeric_limits<number>::max() << "\n\n";
+	}
+}
+
 int main()
 {
 	std::cout << std::setprecision(10);
@@ -9,40 +44,7 @@ int main()
 	
 	while (std::cout << "# ", std::getline(std::cin, input))
 	{
-		if (input.length() && input[0] == '/')
-		{
-			if (input == "/help")
-			{
-				std::cout
-					<< "\n"
-					<< "  sin(x)    - calculates the sinus of x  \n"
-					<< "  cos(x)    - calculates the cosinus of x\n"
-					<< "  tan(x)    - calculates the tangens of x\n"
-					<< "  log(x)    - calculates the natural logarithm of x\n\n"
-					<< "  sqrt(x)   - calculates the square root of x\n"
-					<< "  pow(x, y) - calculates x high y\n\n"
-					<< "  digsum(x) - calculates the digit sum of x\n"
-					<< "  ceil(x)   - rounds x upward\n"
-					<< "  floor(x)  - rounds x downward\n\n"
-					<< "  e         - eularian number\n"
-					<< "  pi        - circular number pi\n"
-					<< "\n";
-			}
-			else if (input == "/clear")
-			{
-				#ifdef _WIN32
-					system("cls");
-				#else
-					system("clear");
-				#endif
-			}
-			else if (input == "/maxval")
-			{
-				std::cout << "-> " << std::numeric_limits<number>::max() << "\n\n";
-			}
-			
-			continue;
-		}
+		process_command_line(input);
 
 		try
 		{
